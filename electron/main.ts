4d36cf7c4563c646532bbdca9dab7aa757f305f2
Dart Spark <57344246+rubyswolf@users.noreply.github.com>
2025-11-09 14:37:43 +1100

    Slightly less broken

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,10 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// ESM shim for __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Acquire single instance lock
 const gotLock = app.requestSingleInstanceLock();
